5f0ca826b1711db314cd0a364b3135cd590e0624
Fajar Riansyah <fajary781@gmail.com>
2025-12-28 14:54:44 +0700

    feat (web): register question show to route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [QuestionController::class, 'index'])->name('home');
+Route::get('/{question}/{slug}', [QuestionController::class, 'show'])->name('question.show');
 Route::get('/about', null)->name('about');
 
 Route::get('/dashboard', function () {
