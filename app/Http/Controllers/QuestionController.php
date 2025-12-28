745dc2f71b318b4c8f8722c4faae0788d85aa56d
Fajar Riansyah <fajary781@gmail.com>
2025-12-28 21:44:02 +0700

    feat (QuestionController): add store method

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Question;
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class QuestionController extends Controller
 {
@@ -31,5 +33,44 @@ class QuestionController extends Controller
         return view('question.create', [
             'tags' => $tags,
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|min:0|max:255',
+            'body' => 'required|string|min:0',
+            'tags' => 'required|string',
+        ]);
+
+        return DB::transaction(function () use ($request, $validated) {
+            // Create Question
+            $question = Question::create([
+                'user_id' => $request->user()->id,
+                'title' => $validated['title'],
+                'body' => $validated['body'],
+                'slug' => Str::slug($validated['title']),
+            ]);
+
+            // Attach Tags
+            $tags = json_decode($validated['tags']);
+            $tagIds = [];
+            foreach ($tags as $tag) {
+                $tagName = $tag->value;
+                $slug = Str::slug($tagName);
+                $tag = Tag::firstOrCreate(
+                    ['slug' => $slug],
+                    ['name' => $tagName],
+                );
+                $tagIds[] = $tag->id;
+            }
+
+            $question->tags()->sync($tagIds);
+
+            return redirect()->route('question.show', [
+                'question' => $question,
+                'slug' => $question->slug
+            ])->with('success', 'Question created successfully.');
+        });
     }
 }
