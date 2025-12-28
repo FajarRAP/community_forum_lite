77be0cd5b509ba1748f25c8022a033d128bbdec1
Fajar Riansyah <fajary781@gmail.com>
2025-12-28 21:43:25 +0700

    feat (question migration): update questions table structure for improved data handling

--- database/migrations/2025_12_28_044105_create_questions_table.php
+++ database/migrations/2025_12_28_044105_create_questions_table.php
@@ -14,9 +14,17 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('title');
-            $table->string('body');
-            $table->string('slug')->unique();
+            $table->string('title')->index();
+            $table->text('body');
+            $table->string('slug');
+
+            $table->unsignedInteger('views')->default(0);
+            $table->integer('answers_count')->default(0);
+            $table->integer('votes_count')->default(0);
+
+            $table->unsignedBigInteger('best_answer_id')->nullable();
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
