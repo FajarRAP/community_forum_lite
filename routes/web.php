2adf46e4c4907f978cc0f4af7c2320044e5e5bf9
Fajar Riansyah <fajary781@gmail.com>
2025-12-28 15:35:48 +0700

    refactor (routes): reorganize routes for clarity and add redirect from root to questions

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,18 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [QuestionController::class, 'index'])->name('home');
-Route::get('/{question}/{slug}', [QuestionController::class, 'show'])->name('question.show');
+Route::get('/', fn() => redirect('/questions'));
+
+Route::get('/questions', [QuestionController::class, 'index'])->name('home');
+Route::get('/questions/{question}/{slug}', [QuestionController::class, 'show'])->name('question.show');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/questions/create', [QuestionController::class, 'create'])->name('question.create');
+    Route::get('/questions/{question}/edit', [QuestionController::class, 'edit'])->name('question.edit');
+    Route::post('/questions', [QuestionController::class, 'store'])->name('question.store');
+    Route::patch('/questions/{question}', [QuestionController::class, 'update'])->name('question.update');
+    Route::delete('/questions/{question}', [QuestionController::class, 'destroy'])->name('question.destroy');
+});
+
 Route::get('/about', null)->name('about');
 
 Route::get('/dashboard', function () {
