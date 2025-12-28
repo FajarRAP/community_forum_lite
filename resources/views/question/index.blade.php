85d7bdb3bdca29b1b21d9baaccee3c9498855ccc
Fajar Riansyah <fajary781@gmail.com>
2025-12-28 15:22:06 +0700

    refactor (question index): improve layout and typography for better readability

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,17 +1,19 @@
 <x-app-layout>
-    <div class="max-w-4xl mx-auto py-4 bg-white min-h-screen">
-        <div class="flex justify-between text-2xl font-medium px-6">
+    <div class="max-w-4xl mx-auto py-4 bg-white min-h-screen px-4 sm:px-6">
+        <div class="flex justify-between text-xl font-medium sm:text-2xl">
             <h1>{{ __('Newest Questions') }}</h1>
             <x-primary-button>{{ __('Ask Questions') }}</x-primary-button>
         </div>
 
-        <p class="mt-8 px-6 text-lg">{{ $question_count }} {{ __('questions') }}</p>
+        <p class="mt-8 text-base sm:text-lg">{{ $question_count }} {{ __('questions') }}</p>
 
-        <div class="mt-8">
+        <div class="divide-y divide-gray-200 border-y my-8">
             @foreach ($questions as $question)
                 <x-questions.item :$question />
             @endforeach
         </div>
+
+        {{ $questions->links('components.pagination.pagination') }}
     </div>
     <x-footer />
 </x-app-layout>
