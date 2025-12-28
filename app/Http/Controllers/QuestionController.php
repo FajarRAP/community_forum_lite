8344f25e82de15ce2ef5747beac50643f91a0179
Fajar Riansyah <fajary781@gmail.com>
2025-12-28 14:25:19 +0700

    feat (QuestionController): update index method to retrieve and pass questions to the view

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
     public function index()
     {
-        return view('question.index');
+        $questions = Question::all();
+        return view('question.index', [
+            'questions' => $questions,
+            'question_count' => $questions->count(),
+        ]);
     }
 }
