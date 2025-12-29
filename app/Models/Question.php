1fcd0f15b3ff6b67a06f21728e89c849f3c78862
Fajar Riansyah <fajary781@gmail.com>
2025-12-29 13:42:52 +0700

    feat (Question): add incrementViewCount method

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,12 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Question extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['user_id', 'title', 'body', 'slug'];
+
+    public function incrementViewCount(): void
+    {
+        DB::table('questions')->where('id', $this->id)->increment('views');
+        $this->views++;
+    }
 
     public function user()
     {
