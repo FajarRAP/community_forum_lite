a75f8e83cd99e625f4e2c5568e48395049001c06
Fajar Riansyah <fajary781@gmail.com>
2026-01-01 22:55:03 +0700

    feat: add answer component

---
+++ resources/views/components/questions/answer.blade.php
@@ -0,0 +1,24 @@
+@props(['answer'])
+
+<div class="flex gap-3 py-4">
+    <div>
+        {{-- {{ $question->isUpvotedBy(auth()->user()) ? 'bg-orange-100 border-orange-500 text-orange-600' : 'text-gray-400' }} --}}
+        <x-questions.up-vote :action="route('vote', ['type' => 'answers', 'id' => $answer->id])" />
+
+        <div class="text-2xl font-bold my-2 text-center text-gray-700">
+            {{ $answer->votes_count }}
+        </div>
+
+        {{-- {{ $question->isDownvotedBy(auth()->user()) ? 'bg-orange-100 border-orange-500 text-orange-600' : 'text-gray-400' }} --}}
+        <x-questions.down-vote :action="route('vote', ['type' => 'answers', 'id' => $answer->id])" />
+    </div>
+    <div class="space-y-4">
+        <p>{{ $answer->body }}</p>
+        <p class="text-sm text-gray-500 text-right">{{ __('answered') }}
+            {{ $answer->created_at->format('M d, Y') }}
+            {{ __('at') }}
+            {{ $answer->created_at->format('H:i') }}
+            <span class="font-medium text-indigo-600">{{ $answer->user->name }}</span>
+        </p>
+    </div>
+</div>
