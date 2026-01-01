3fcc2a20b8cff4c8bfb8b307950fcd0ce8358475
Fajar Riansyah <fajary781@gmail.com>
2026-01-01 21:48:36 +0700

    feat (question item): enhance layout and display of question details with views and answers count

--- resources/views/components/questions/item.blade.php
+++ resources/views/components/questions/item.blade.php
@@ -1,25 +1,49 @@
 @props(['question'])
 
-<div class="py-4">
-    {{-- Title --}}
-    <a href="{{ route('question.show', ['question' => $question, 'slug' => $question->slug]) }}"
-        class="text-lg font-semibold text-indigo-500 hover:text-indigo-700 transition sm:text-xl">{{ $question->title }}</a>
+@php
+    $color = '';
+    if ($question->views > 100) {
+        $color = 'text-red-600';
+    } elseif ($question->views > 50) {
+        $color = 'text-amber-600';
+    } else {
+        $color = 'text-gray-500';
+    }
+@endphp
 
-    {{-- Body --}}
-    <p class="mt-2 text-gray-600 leading-relaxed text-sm sm:text-base">{{ Str::limit($question->body, 150) }}</p>
+<div class="py-4 flex gap-3">
+    <div class="flex flex-col gap-1.5 items-end basis-1/5">
+        <p class="text-sm">{{ $question->votes_count . ' ' . __('votes') }} </p>
+        @if ($question->answers_count > 0)
+            <p class="text-sm text-green-700 border border-green-700 rounded px-1.5">
+                {{ $question->answers_count . ' ' . __('answers') }}
+            </p>
+        @else
+            <p class="text-sm text-gray-500">{{ $question->answers_count . ' ' . __('answers') }}</p>
+        @endif
+        <p class="text-sm {{ $color }}">{{ $question->viewsDisplay() . ' ' . __('views') }} </p>
+    </div>
+    <div>
+        {{-- Title --}}
+        <a href="{{ route('question.show', ['question' => $question, 'slug' => $question->slug]) }}"
+            class="text-lg font-semibold text-indigo-500 hover:text-indigo-700 transition sm:text-xl">{{ $question->title }}</a>
 
-    {{-- Meta Footer --}}
-    <div class="mt-3 flex items-center justify-between text-gray-500 gap-1 text-xs sm:text-sm">
-        <div class="flex gap-1">
-            @foreach ($question->tags as $tag)
-                <x-questions.tag :$tag />
-            @endforeach
+        {{-- Body --}}
+        <p class="mt-2 text-gray-600 leading-relaxed text-sm sm:text-base">{{ $question->bodyPreview() }}</p>
+
+        {{-- Meta Footer --}}
+        <div class="mt-3 flex items-center justify-between text-gray-500 gap-1 text-xs sm:text-sm">
+            <div class="flex gap-1">
+                @foreach ($question->tags as $tag)
+                    <x-questions.tag :$tag />
+                @endforeach
+            </div>
+            <p>
+                <span>{{ __('asked') }}</span>
+                <span class="font-medium text-gray-900">{{ $question->created_at->diffForHumans() }}</span>
+                <span>{{ __('by') }}</span>
+                <span class="font-medium text-indigo-600">{{ $question->user->name }}</span>
+            </p>
         </div>
-        <p>
-            <span>{{ __('asked') }}</span>
-            <span class="font-medium text-gray-900">{{ $question->created_at->diffForHumans() }}</span>
-            <span>{{ __('by') }}</span>
-            <span class="font-medium text-indigo-600">{{ $question->user->name }}</span>
-        </p>
     </div>
 </div>
