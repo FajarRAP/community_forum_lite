e21c5b2c56b8972b390625bb01cb376633067245
Fajar Riansyah <fajary781@gmail.com>
2026-01-04 00:14:42 +0700

    feat: add best mark component

---
+++ resources/views/components/answer/best-mark.blade.php
@@ -0,0 +1,22 @@
+@props(['answer', 'question'])
+
+@php
+    $isBest = $answer->isBestAnswer($question);
+    $canMark = auth()->user()?->can('markAsBest', $answer);
+    $classes = $isBest ? 'text-green-600 bg-green-100 hover:bg-green-200' : 'text-gray-300 hover:text-green-500';
+@endphp
+
+@if ($canMark)
+    <form action="{{ route('answers.markAsBest', ['answer' => $answer]) }}" method="POST">
+        @csrf
+        @method('PATCH')
+        <button type="submit" title="Mark as best answer"
+            class="p-1 rounded-full transition-colors duration-200 {{ $classes }}">
+            <x-svgs.check />
+        </button>
+    </form>
+@elseif ($isBest)
+    <div class="text-green-600" title="The question owner accepted this as the best answer">
+        <x-svgs.check />
+    </div>
+@endif
