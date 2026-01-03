94ba1d50f5d9538676f439debed7c0bc2771d9de
Fajar Riansyah <fajary781@gmail.com>
2026-01-03 23:59:17 +0700

    feat: reorganize routes for dashboard and about pages

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::fallback(fn() => redirect('/questions'));
 
 Route::get('/', fn() => redirect('/questions'));
+Route::get('/dashboard', fn() => redirect('/questions'))->name('dashboard');
+Route::get('/about', null)->name('about');
 
 Route::get('/questions', [QuestionController::class, 'index'])->name('home');
 Route::get('/questions/{question}/{slug}', [QuestionController::class, 'show'])->name('question.show');
@@ -27,18 +29,11 @@ Route::middleware(['auth'])->group(function () {
         ->name('answers.markAsBest');
 
     Route::post('/vote/{type}/{id}', VoteController::class)->name('vote');
-});
 
-Route::get('/about', null)->name('about');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__ . '/auth.php';
