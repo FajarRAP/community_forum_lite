be5a2ba1450809cf588b883f69f3e88e0bf8f8b3
Fajar Riansyah <fajary781@gmail.com>
2026-01-10 12:33:48 +0700

    feat (QuestionPolicy): implement update and delete policies

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Question;
 use App\Models\User;
 
 class QuestionPolicy
@@ -9,5 +10,15 @@ class QuestionPolicy
     public function create(User $user): bool
     {
         return true;
+    }
+
+    public function update(User $user, Question $question): bool
+    {
+        return $user->id === $question->user_id;
+    }
+
+    public function delete(User $user, Question $question): bool
+    {
+        return $user->id === $question->user_id;
     }
 }
