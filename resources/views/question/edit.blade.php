d8d2bda3903fe7f9c193b46dabaac7787049dbec
Fajar Riansyah <fajary781@gmail.com>
2026-01-10 13:39:08 +0700

    feat (edit question): create edit question view with form and footer

---
+++ resources/views/question/edit.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="max-w-4xl mx-auto py-4 bg-white min-h-screen px-4 sm:px-6">
+        <div class="flex justify-between items-end text-xl font-medium sm:text-2xl">
+            <h1>{{ __('Edit Question') }}</h1>
+            <p class="font-light text-sm text-gray-700">{{ __('Required fields') }}<x-red-asterisk /></p>
+        </div>
+        @include('question.partials.question-form', ['question' => $question, 'tags' => $tags])
+    </div>
+
+    <x-footer />
+</x-app-layout>
