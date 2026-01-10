ec9af1624a0bd7554d43207c7760bbfe5b688097
Fajar Riansyah <fajary781@gmail.com>
2026-01-10 13:38:55 +0700

    feat (create question): refactor question form to use partial view for better maintainability

--- resources/views/question/create.blade.php
+++ resources/views/question/create.blade.php
@@ -1,71 +1,11 @@
 <x-app-layout>
-    @push('styles')
-        <link href="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.css" rel="stylesheet" type="text/css" />
-        <script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify"></script>
-
-        <style>
-            :root {
-                --tagify-dd-color-primary: #6366f1;
-            }
-        </style>
-    @endpush
-
     <div class="max-w-4xl mx-auto py-4 bg-white min-h-screen px-4 sm:px-6">
         <div class="flex justify-between items-end text-xl font-medium sm:text-2xl">
             <h1>{{ __('Ask Question') }}</h1>
             <p class="font-light text-sm text-gray-700">{{ __('Required fields') }}<x-red-asterisk /></p>
         </div>
-        <form method="POST" action="{{ route('question.store') }}" class="mt-8">
-            @csrf
-            <div class="mt-8">
-                <div class="flex">
-                    <x-input-label for="title" :value="__('Title')" class="font-bold !text-base" />
-                    <x-red-asterisk />
-                </div>
-                <span
-                    class="text-gray-500 text-xs">{{ __('Be specific and imagine you\'re asking a question to another person. Min 15 characters.') }}</span>
-                <x-text-input id="title" class="block mt-1 w-full" name="title" :value="old('title')" required />
-                <x-input-error :messages="$errors->get('title')" class="mt-2" />
-            </div>
-            <div class="mt-8">
-                <div class="flex">
-                    <x-input-label for="body" :value="__('Body')" class="font-bold !text-base" />
-                    <x-red-asterisk />
-                </div>
-                <span
-                    class="text-gray-500 text-xs">{{ __('Include all the information someone would need to answer your question. Min 220 characters.') }}</span>
-                <x-text-input id="body" class="block mt-1 w-full" name="body" :value="old('body')" required />
-                <x-input-error :messages="$errors->get('body')" class="mt-2" />
-            </div>
-            <div class="mt-8">
-                <div class="flex">
-                    <x-input-label for="tags" :value="__('Tags')" class="font-bold !text-base" />
-                    <x-red-asterisk />
-                </div>
-                <span
-                    class="text-gray-500 text-xs">{{ __('Add up to 5 tags to describe what your question is about. Start typing to see suggestions.') }}</span>
-                <x-text-input id="tags" class="mt-1 w-full" name="tags" :value="old('tags')" required />
-                <x-input-error :messages="$errors->get('tags')" class="mt-2" />
-            </div>
-
-            <x-primary-button class="mt-8">{{ __('Post Your Question') }}</x-primary-button>
-        </form>
+        @include('question.partials.question-form', ['tags' => $tags])
     </div>
-    <x-footer />
 
-    @push('scripts')
-        <script>
-            const whitelist = @json($tags);
-            const tagInput = document.querySelector('input[name=tags]');
-            const tagify = new Tagify(tagInput, {
-                whitelist: whitelist,
-                maxTags: 5,
-                dropdown: {
-                    enabled: 0,
-                    maxItems: 5,
-                    closeOnSelect: false,
-                }
-            });
-        </script>
-    @endpush
+    <x-footer />
 </x-app-layout>
