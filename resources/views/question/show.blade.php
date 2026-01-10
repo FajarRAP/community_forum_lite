3f884813a666075827ea47a87ce08653dff7b9a9
Fajar Riansyah <fajary781@gmail.com>
2026-01-10 12:35:34 +0700

    feat (QuestionShow): add edit and delete options for questions with confirmation modal

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -18,7 +18,7 @@
             </p>
             <p>
                 <span>{{ __('Viewed') }}</span>
-                <span class="text-gray-900">{{ Number::abbreviate($question->views) . ' ' . __('times') }}
+                <span class="text-gray-900">{{ $question->viewsDisplay() . ' ' . __('times') }}
                 </span>
             </p>
         </div>
@@ -26,6 +26,20 @@
         <div class="my-4 space-y-6">
             <div class="border-t border-gray-200">
                 <x-questions.question :$question />
+
+                @can(['update', 'delete'], $question)
+                    <div class="flex items-center gap-4 mt-4 mb-6">
+                        <a href="{{ route('question.edit', $question->slug) }}"
+                            class="text-gray-500 hover:text-indigo-600 font-medium text-sm transition">
+                            {{ __('Edit') }}
+                        </a>
+
+                        <button x-data x-on:click.prevent="$dispatch('open-modal', 'confirm-question-deletion')"
+                            class="text-red-500 hover:text-red-700 font-medium text-sm transition">
+                            {{ __('Delete') }}
+                        </button>
+                    </div>
+                @endcan
 
                 @if ($question->answers_count > 0)
                     <p class="text-xl">{{ $question->answers_count }} {{ __('Answers') }}</p>
@@ -69,5 +83,31 @@
             @endguest
         </div>
     </div>
+
     <x-footer />
+
+    <x-modal name="confirm-question-deletion" focusable>
+        <form method="post" action="{{ route('question.destroy', $question) }}" class="p-6">
+            @csrf
+            @method('delete')
+
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Are you sure you want to delete this question?') }}
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600">
+                {{ __('Once this question is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete this question.') }}
+            </p>
+
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-danger-button class="ms-3">
+                    {{ __('Delete Question') }}
+                </x-danger-button>
+            </div>
+        </form>
+    </x-modal>
 </x-app-layout>
